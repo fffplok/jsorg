b60f7bf39f0d3bb450e4ed20161d5d54d4c79379
fffplok <fffplok@gmail.com>
2016-03-22 15:01:59 -0500

    feature:highlight person selected and position in carousel

--- js/carousel.js
+++ js/carousel.js
@@ -26,6 +26,26 @@ var Carousel = (function() {
 		$items.css({ left: (-position) + "px" });
 	}
 
+	function scrollTo(id) {
+		console.log('scrollTo, arguments, id:', arguments, id);
+	}
+
+	function hilightPerson(b) {
+		var s = b ? '*' : '';
+ 		$personSelected.attr('data-attr', s);
+	}
+
+	// for this exercise, both carousel and details can emit a person-selected event
+	// take out the code specific to showing person selected from personClicked and
+	// put here
+	function personSelected(id) {
+		if ($personSelected) hilightPerson(false); //not til first click will there be a $personSelected
+		$personSelected = $("[rel=js-item-"+id+"]");
+		hilightPerson(true);
+		position = Math.min(maxPosition,$personSelected.position().left);
+		$items.css({ left: (-position) + "px" });
+	}
+
 	function personClicked(e) {
 		e.preventDefault();
 		e.stopPropagation();
@@ -35,9 +55,6 @@ var Carousel = (function() {
 		id = s.slice(s.lastIndexOf('-')+1);
 
 		EVT.emit('person-selected', id);
-
-		//Details.loadPerson(id);
-		//debugger;
 	}
 
 	function init() {
@@ -56,11 +73,12 @@ var Carousel = (function() {
 		$("[rel=js-content]").on("click","> [rel^=js-item]", personClicked);
 	}
 
-	var $content, $items, $left, $right;
+	var $content, $items, $left, $right, $personSelected;
 
 	var contentWidth, itemsWidth, position, maxPosition, id;
 
 	EVT.on('init', init);
+	EVT.on('person-selected', personSelected);
 
 	return {};
 })();
